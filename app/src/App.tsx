f86b95a3fab8169e2614becb19c5788f5cd75b4b
ChiiYuen <chii.yuen@hotmail.com>
2025-04-05 17:08:02 +0800

    Refactor code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,13 @@ import Chat from './components/chat';
 import ChatProvider from './components/chat-provider';
 import Header from './components/header';
 import SolanaProvider from './components/solana-provider';
-import ThemeProvider from './components/theme-provider';
+import ThemeProvider, { useTheme } from './components/theme-provider';
 import { Toaster } from './components/ui';
 import { SWRConfig } from 'swr';
 
 export default function App() {
+  const { theme } = useTheme();
+
   return (
     <>
       <SWRConfig value={{ suspense: false, revalidateOnFocus: false }}>
@@ -15,23 +17,23 @@ export default function App() {
             <ChatProvider>
               <Header />
               <Chat />
+              <Toaster
+                richColors
+                closeButton
+                theme={theme}
+                toastOptions={{
+                  classNames: {
+                    error: 'bg-red-400',
+                    success: 'text-green-400',
+                    warning: 'text-yellow-400',
+                    info: 'bg-blue-400',
+                  },
+                }}
+              />
             </ChatProvider>
           </SolanaProvider>
         </ThemeProvider>
       </SWRConfig>
-      <Toaster
-        richColors
-        closeButton
-        theme="system"
-        toastOptions={{
-          classNames: {
-            error: 'bg-red-400',
-            success: 'text-green-400',
-            warning: 'text-yellow-400',
-            info: 'bg-blue-400',
-          },
-        }}
-      />
     </>
   );
 }
