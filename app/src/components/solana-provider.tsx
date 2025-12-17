185dabb865c18afce001117657951cee7c0ddd34
Chii Yuen <chii.yuen@hotmail.com>
2025-12-18 01:17:03 +0800

    Register LazorKit wallet

--- app/src/components/solana-provider.tsx
+++ app/src/components/solana-provider.tsx
@@ -3,31 +3,27 @@ import {
   WalletProvider,
 } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import {
-  BitgetWalletAdapter,
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  SalmonWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { ReactNode, useMemo } from 'react';
+import { ReactNode, useEffect } from 'react';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { connection } from '@/lib/constants';
+import { registerLazorkitWallet } from '@lazorkit/wallet';
 
 export function SolanaProvider({ children }: { children: ReactNode }) {
-  const endpoint = useMemo(() => connection.rpcEndpoint, []);
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new BitgetWalletAdapter(),
-      new SalmonWalletAdapter(),
-    ],
-    []
-  );
+  useEffect(() => {
+    registerLazorkitWallet({
+      rpcUrl: import.meta.env.VITE_RPC_URL,
+      portalUrl: import.meta.env.VITE_LAZORKIT_PORTAL_URL,
+      paymasterConfig: {
+        paymasterUrl: import.meta.env.VITE_LAZORKIT_PAYMASTER_URL,
+        apiKey: import.meta.env.VITE_LAZORKIT_API_KEY,
+      },
+      clusterSimulation: import.meta.env.VITE_RPC_CLUSTER,
+    });
+  }, []);
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
+    <ConnectionProvider endpoint={connection.rpcEndpoint}>
+      <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
